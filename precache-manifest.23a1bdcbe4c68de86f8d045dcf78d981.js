a4bebcc568e43633fec8b3d0df1544a87e13faf4
Aldegid <aldegid@gmail.com>
2019-06-27 16:21:33 +0300

    Updates

---
+++ precache-manifest.23a1bdcbe4c68de86f8d045dcf78d981.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90f94ef0ec367ac49fe5f7a8129d46e3",
+    "url": "/react-blog-app/index.html"
+  },
+  {
+    "revision": "989e48baca314b5142bc",
+    "url": "/react-blog-app/static/css/main.53110818.chunk.css"
+  },
+  {
+    "revision": "a9895d7a80022a15f5c6",
+    "url": "/react-blog-app/static/js/2.4929b02f.chunk.js"
+  },
+  {
+    "revision": "989e48baca314b5142bc",
+    "url": "/react-blog-app/static/js/main.38fc94ac.chunk.js"
+  },
+  {
+    "revision": "802bccb5491fd07e1854",
+    "url": "/react-blog-app/static/js/runtime~main.cb2c880d.js"
+  }
+]);
